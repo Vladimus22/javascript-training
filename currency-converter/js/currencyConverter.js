993438fd518784f5721d6c52ee80dde3b8ebf591
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-06-12 17:03:39 +0300

    Upd CurrencyConverter
    Add API request

---
+++ currency-converter/js/currencyConverter.js
@@ -0,0 +1,89 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let firstValue = document.getElementById("first_value");
+let checkUnitIsActive = true;
+
+document.getElementById("one").addEventListener("click", inputOne);
+document.getElementById("two").addEventListener("click", inputTwo);
+document.getElementById("three").addEventListener("click", inputThree);
+document.getElementById("four").addEventListener("click", inputFour);
+document.getElementById("five").addEventListener("click", inputFive);
+document.getElementById("six").addEventListener("click", inputSix);
+document.getElementById("seven").addEventListener("click", inputSeven);
+document.getElementById("eight").addEventListener("click", inputEight);
+document.getElementById("nine").addEventListener("click", inputNine);
+
+function inputOne() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[0];
+    }   // } else {
+    //    second_unit.value = second_unit.value + numbers[0];
+    // }   
+}
+function inputTwo() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[1];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[1];
+    // }   
+}
+function inputThree() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[2];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[2];
+    // }   
+}
+function inputFour() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[3];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[3];
+    // }   
+}
+function inputFive() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[4];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[4];
+    // }   
+}
+function inputSix() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[5];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[5];
+    // }   
+}
+function inputSeven() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[6];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[6];
+    // }   
+}
+function inputEight() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[7];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[7];
+    // }   
+}
+function inputNine() {
+    if (checkUnitIsActive == true) {
+        firstValue.value = firstValue.value + numbers[8];
+    }
+    // } else {
+    //    second_unit.value = second_unit.value + numbers[8];
+    // }   
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/USD")
+    .then(res => res.json())
+    .then(data => console.log(data))
