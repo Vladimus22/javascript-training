397e3158be41528b9a4382312c527e53f52a9930
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-06-21 23:31:39 +0300

    Update functionsTraining.js

--- basic-scripts/functionsTraining.js
+++ basic-scripts/functionsTraining.js
@@ -14,3 +14,23 @@ alert(min(1,1))
 // function min(a, b) {
 //     return a < b ? a : b;
 //   }
+
+function pow(x,n){
+   let result = x;
+
+   for(let i = 1; i < n; i++){
+        result *= x;
+   }
+   return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if(n < 1){
+    alert('Error');
+}else {
+    alert( pow(x,n));
+}
+
+alert(pow(3, 2));
