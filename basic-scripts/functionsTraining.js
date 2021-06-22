eda919d0f56808cfc0fe0b62a8734f522c5c90e6
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-06-22 15:23:16 +0300

    Update functionsTraining.js
    Practiced with function expression, arrow functions

--- basic-scripts/functionsTraining.js
+++ basic-scripts/functionsTraining.js
@@ -34,3 +34,65 @@ if(n < 1){
 }
 
 alert(pow(3, 2));
+
+//Function Callback
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    alert( "You agreed" );
+  }
+  
+  function showCancel() {
+    alert( "You canceled" );
+  }
+  
+  ask("Are you sure?", showOk, showCancel);
+
+//Function Expression 
+let sum = function(a, b) {
+   return a + b;
+};
+
+//Arrow Function
+let sum = (a, b) => a + b;
+alert( sum(1, 2) );
+//Arrow Function without arguments
+let sayHi = () => alert("Hello!");
+sayHi();
+//Arrow Function without brackets if we have only one argument
+let double = n => n * 2;
+
+alert( double(3) ); // 6
+//Arrow Function with operator ?
+let age = prompt("Сколько Вам лет?", 18);
+
+let welcome = (age < 18) ?
+  () => alert('Привет') :
+  () => alert("Здравствуйте!");
+
+welcome();
+
+//Task: Function Expression -> Arrow Function
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   ask(
+//     "Вы согласны?",
+//     function() { alert("Вы согласились."); },
+//     function() { alert("Вы отменили выполнение."); }
+//   );
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+ask (
+    'Are you sure?',
+    () => alert('You agreed'),
+    () => alert('You canceled')
+);
