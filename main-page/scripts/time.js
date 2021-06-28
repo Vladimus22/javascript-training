bd45996de884c7aed7a22f5367bd1bf57d98b7b8
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-06-29 01:32:57 +0300

    upd time.js
    Added current time on web page

--- main-page/scripts/time.js
+++ main-page/scripts/time.js
@@ -1,11 +1,21 @@
-let currentTime = new Date();
-console.log(currentTime);
-let currentDate;
-fetch ("http://worldtimeapi.org/api/timezone/Europe/Warsaw")
-    .then(res => res.json())
-    .then(data => currentDate = data.datetime)
+// let currentTime = new Date();
+// console.log(currentTime);
+// let currentDate;
+// fetch ("http://worldtimeapi.org/api/timezone/Europe/Warsaw")
+//     .then(res => res.json())
+//     .then(data => currentDate = data.datetime)
 
-console.log(currentDate);
-let str = toString(currentDate);
-let strdate = str.split(/[:.]/);
-console.log(strdate);
+// console.log(currentDate);
+// let str = toString(currentDate);
+// let strdate = str.split(/[:.]/);
+// console.log(strdate);
+window.onload = setInterval(function() {
+    let timeZone = 1;
+
+    let currentTime = new Date();
+    let currentHourMinutesSeconds = (currentTime.getHours() - timeZone) + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
+    
+    document.getElementById('current-time-container').innerHTML = "Current Local Time in Vladgard: " + currentHourMinutesSeconds;
+
+}, 1000);
+
