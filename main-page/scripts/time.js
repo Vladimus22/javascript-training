0e385dd140f572ad6f84fd8f2f5fc0f6b395f96f
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-06-30 17:18:14 +0300

    update time.js
    Build with geonames.org API request that show current time what I need

--- main-page/scripts/time.js
+++ main-page/scripts/time.js
@@ -9,17 +9,34 @@
 // let str = toString(currentDate);
 // let strdate = str.split(/[:.]/);
 // console.log(strdate);
-window.onload = setInterval(function() {
-    let timeZone = 1;
 
-    let currentTime = new Date();
-    let currentHourMinutesSeconds = (currentTime.getHours() - timeZone) + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
+// window.onload = setInterval(function() {
+//     let timeZone = 1;
+
+//     let currentTime = new Date();
+//     let currentHourMinutesSeconds = (currentTime.getHours() - timeZone) + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
     
-    document.getElementById('current-time-container').innerHTML = "Current Local Time in Vladgard: " + currentHourMinutesSeconds;
+//     document.getElementById('current-time-container').innerHTML = "Current Local Time in Vladgard: " + currentHourMinutesSeconds;
 
+// }, 1000);
+
+// function showMessage(message) {
+//     message = 'Sorry, unfortunately I have not completed this page. Please, come back soon.';
+//     return alert(message);
+// }
+let getTime;
+
+fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
+    .then(res => res.json())
+    .then(data => getTime = data.time)
+
+fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
+    .then(res => res.json())
+    .then(data2 => console.log(data2))
+
+window.onload = setInterval(function () {
+    let currentTime = getTime;
+    let message = 'Current Local Time in Vladgard: ';
+    document.getElementById('current-time-container').innerHTML = message + currentTime ;
 }, 1000);
 
-function showMessage(message) {
-    message = 'Sorry, unfortunately I have not completed this page. Please, come back soon.';
-    return alert(message);
-}
