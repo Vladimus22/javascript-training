b5000f7c73719028da2d2f47f74b06c5bd1a61ab
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-07-01 14:46:56 +0300

    Update time.js

--- main-page/scripts/time.js
+++ main-page/scripts/time.js
@@ -10,33 +10,37 @@
 // let strdate = str.split(/[:.]/);
 // console.log(strdate);
 
-// window.onload = setInterval(function() {
-//     let timeZone = 1;
+window.onload = setInterval(function() {
+    let timeZone = 1;
 
-//     let currentTime = new Date();
-//     let currentHourMinutesSeconds = (currentTime.getHours() - timeZone) + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
+    let currentTime = new Date();
+    let currentHourMinutesSeconds = (currentTime.getHours() - timeZone) + ':' + currentTime.getMinutes() + ':' + currentTime.getSeconds();
     
-//     document.getElementById('current-time-container').innerHTML = "Current Local Time in Vladgard: " + currentHourMinutesSeconds;
+    document.getElementById('current-time-container').innerHTML = "Current Local Time in Vladgard: " + currentHourMinutesSeconds;
 
+}, 1000);
+
+function showMessage(message) {
+    message = 'Sorry, unfortunately I have not completed this page. Please, come back soon.';
+    return alert(message);
+}
+
+//Working but GitHub pages block http unsecurity content
+// let getTime;
+
+// fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
+//     .then(res => res.json())
+//     .then(data => getTime = data.time)
+
+// fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
+//     .then(res => res.json())
+//     .then(data2 => console.log(data2))
+
+// window.onload = setInterval(function () {
+//     let currentTime = getTime;
+//     let message = 'Current Local Time in Vladgard: ';
+//     document.getElementById('current-time-container').innerHTML = message + currentTime ;
 // }, 1000);
 
-// function showMessage(message) {
-//     message = 'Sorry, unfortunately I have not completed this page. Please, come back soon.';
-//     return alert(message);
-// }
-let getTime;
 
-fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
-    .then(res => res.json())
-    .then(data => getTime = data.time)
-
-fetch ('http://api.geonames.org/timezoneJSON?formatted=true&lat=52.13&lng=21.08&username=vladimus&style=full')
-    .then(res => res.json())
-    .then(data2 => console.log(data2))
-
-window.onload = setInterval(function () {
-    let currentTime = getTime;
-    let message = 'Current Local Time in Vladgard: ';
-    document.getElementById('current-time-container').innerHTML = message + currentTime ;
-}, 1000);
 
